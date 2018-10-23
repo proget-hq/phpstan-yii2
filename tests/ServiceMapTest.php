a7a118f78994d4c612d9bd7fa40324fb63d5f305
Marcin Michalski <evulmastah@gmail.com>
2018-10-23 21:49:18 +0200

    Add ServiceMap tests

---
+++ tests/ServiceMapTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Proget\Tests\PHPStan\Yii2;
+
+use PhpParser\Node\Scalar\String_;
+use PHPUnit\Framework\TestCase;
+use Proget\PHPStan\Yii2\ServiceMap;
+
+final class ServiceMapTest extends TestCase
+{
+    public function testThrowExceptionWhenConfigurationFileDoesNotExist(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Provided config path invalid-path must exist');
+
+        new ServiceMap('invalid-path');
+    }
+
+    public function testThrowExceptionWhenClosureServiceHasMissingReturnType(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Please provide return type for no-return-type service closure');
+
+        new ServiceMap(__DIR__.DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR.'yii-config-invalid.php');
+    }
+
+    public function testItLoadsServices(): void
+    {
+        $serviceMap = new ServiceMap(__DIR__.DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR.'yii-config-valid.php');
+
+        $this->assertSame(\SplStack::class, $serviceMap->getServiceClassFromNode(new String_('closure')));
+        $this->assertSame(\SplObjectStorage::class, $serviceMap->getServiceClassFromNode(new String_('service')));
+        $this->assertSame(\SplFileInfo::class, $serviceMap->getServiceClassFromNode(new String_('nested-service-class')));
+    }
+}
